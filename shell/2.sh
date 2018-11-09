97e5a307fdaca7e998f522344f0c1eca2afe201f
gudongxian <gudongxian@qq.com>
2018-11-09 18:54:21 +0800

    细节完善

--- shell/2.sh
+++ shell/2.sh
@@ -10,6 +10,7 @@ for file in $(ls /etc);do
 	echo "2 $file"
 done
 
-for skill in Ada Coffe Action Java;do
+for skill in Ada Coffe Action Java;
+do
 	echo "I am good at ${skill}Script"
 done
---
+++ shell/4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo $#
+
+set a b c
+
+echo $#
