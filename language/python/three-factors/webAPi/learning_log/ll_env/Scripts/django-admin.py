790fa7221f5d693ba0703d20f64b9ab96848c1c6
gudongxian <gudongxian@qq.com>
2019-11-03 16:42:36 +0800

    删除临时文件

--- language/python/three-factors/webAPi/learning_log/ll_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\gudongxian\desktop\python_base\webapi\learning_log\ll_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
